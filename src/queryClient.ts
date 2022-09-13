ad122b41a8b3dab35f14807dbdacb41e544bfd3c
donlinoxd <don.lino.xd@gmail.com>
2022-09-13 08:21:51 +0800

    Initial commit for client side

---
+++ src/queryClient.ts
@@ -0,0 +1,16 @@
+import { QueryClient } from "@tanstack/react-query";
+import { GraphQLClient } from "graphql-request";
+import { cookies } from "./utils/cookie.util";
+
+const accessToken = cookies.get("authorization") as string;
+
+export const queryClient = new QueryClient();
+export const reqClient = new GraphQLClient(
+  "https://captured-server.herokuapp.com/graphql",
+  {
+    headers: {
+      authorization: accessToken ?? "",
+    },
+    credentials: "include",
+  }
+);
